85fc2a9fa86f26b7c4aced11120174f05b73759a
SuatOzcan <41187131+SuatOzcan@users.noreply.github.com>
2023-10-27 19:32:39 +0300

    Create a multiply function and write its tests.

--- functions.py
+++ functions.py
@@ -4,3 +4,14 @@ def divide(dividend : Union[int,float], divisor: Union[int, float]):
     if divisor == 0:
         raise ValueError("The divisor can not be 0.")
     return dividend / divisor
+
+def multiply(*args : Union[int, float]):
+    if len(args) == 0:
+        raise ValueError("At least one value to multiply has to be passed.")
+    
+    total = 1
+    for arg in args:
+        total *= arg
+    
+    return total
+    
--- test_functions.py
+++ test_functions.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from functions import divide
+from functions import divide, multiply
 
 class TestFunctions(TestCase):
     def test_divide_result(self):
@@ -26,3 +26,36 @@ class TestFunctions(TestCase):
             divide(25, 0)
         
         #self.assertRaises(ValueError, lambda : divide(25, 0))
+    
+    def test_multiply_empty(self):
+        with self.assertRaises(ValueError):
+            multiply()
+    
+    def test_multiply_single_value(self):
+        expected = 15
+        self.assertEqual(multiply(expected), expected)
+    
+    def test_multiply_zero(self):
+        expected = 0
+        self.assertEqual(multiply(0),expected)
+    
+    def test_multipy_multiple_values(self):
+        inputs =(3,5)
+        expected = 15
+        self.assertEqual(multiply(*inputs), expected)
+    
+    def test_multiply_result_with_zero(self):
+        inputs = (3,5,0)
+        expected = 0
+        self.assertEqual(multiply(*inputs), expected)
+
+    def test_multiply_negative(self):
+        inputs =(3,-5,2)
+        expected = -30
+        self.assertEqual(multiply(*inputs),expected)
+    
+    def test_multiply_floats(self):
+        inputs = (2.0,3.2)
+        expected = 6.4
+        self.assertAlmostEqual(multiply(*inputs),expected, delta =0.000000000000001)
+    
